8fad1dfa2e7cd166a06dd569e345c2b223b7adfc
r-y4n <153890995+r-y4n@users.noreply.github.com>
2024-08-05 15:27:43 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,28 @@
 import { useState, useEffect } from 'react';
 import styles from '../styles/styles.module.css';
 
-const images = [
-  '/images/IMG_4028.jpeg',
-  '/images/IMG_4001.jpeg',
-  '/images/IMG_7634.jpeg'
-];
-
 export default function Home() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [fade, setFade] = useState(true);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch('/api/images');
+        const data = await response.json();
+        setImages(data.map(image => image.url));
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
+  useEffect(() => {
+    if (images.length === 0) return;
+
     const interval = setInterval(() => {
       setFade(false);
       setTimeout(() => {
@@ -19,8 +30,9 @@ export default function Home() {
         setFade(true);
       }, 1000); // Match this duration to the CSS transition duration
     }, 4000); // Change image every 4 seconds (including 1 second for fade out and 1 second for fade in)
+
     return () => clearInterval(interval);
-  }, []);
+  }, [images]);
 
   useEffect(() => {
     const footer = document.createElement('footer');
@@ -46,11 +58,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1>hi</h1>
-      <img 
-        src={images[currentImageIndex]} 
-        alt="If you are seeing this I made an oopsie 😔🙏" 
-        className={`${styles.image} ${fade ? styles['fade-in'] : ''}`} 
-      />
+      {images.length > 0 && (
+        <img 
+          src={images[currentImageIndex]} 
+          alt="If you are seeing this I made an oopsie 😔🙏" 
+          className={`${styles.image} ${fade ? styles['fade-in'] : ''}`} 
+        />
+      )}
       <div className={styles.circ2}></div>
       <div className={styles.circ1}></div>
     </div>
