a0604d44837485fe825cbc109dfa1b0fb7e4717b
r-y4n <153890995+r-y4n@users.noreply.github.com>
2024-08-05 15:26:08 -0500

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,9 +1,12 @@
 // api/db.js
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb+srv://jonesrya001:<Gravityfalls1!!>@cluster0.c6kxeqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
 
 const imageSchema = new mongoose.Schema({
   url: String,
