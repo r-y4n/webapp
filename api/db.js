23c94fbdc0779485779bd8a491bb88ec19a0314d
r-y4n <153890995+r-y4n@users.noreply.github.com>
2024-08-05 15:17:05 -0500

    Update and rename images.js to db.js

---
+++ api/db.js
@@ -0,0 +1,15 @@
+// api/db.js
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://jonesrya001:<pass>@cluster0.c6kxeqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const imageSchema = new mongoose.Schema({
+  url: String,
+  description: String,
+});
+
+const Image = mongoose.model('Image', imageSchema);
+
+module.exports = Image;
