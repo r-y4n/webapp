47f2a1a2654eb3a14390664ab4a2c4d7cd7e93c9
r-y4n <153890995+r-y4n@users.noreply.github.com>
2024-08-08 02:25:38 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -53,6 +53,14 @@ export default function Home() {
       />
       <div className={styles.circ2}></div>
       <div className={styles.circ1}></div>
+        <div className={styles.navbar}>
+        <h1>
+          <a href="/index" style={{ color: '#000000' }}>Home</a>
+        </h1>
+        <h1>
+          <a href="/projects" style={{ color: '#000000' }}>Projects</a>
+        </h1>
+      </div>
     </div>
   );
 }
