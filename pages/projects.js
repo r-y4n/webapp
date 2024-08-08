c1d1a53d1b05f9c93ddfa2d3fae90b6349cbef9e
r-y4n <153890995+r-y4n@users.noreply.github.com>
2024-08-08 01:34:45 -0500

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -1,14 +1,17 @@
 import styles from '../styles/styles.module.css';
 
 export default function Projects() {
-  return(
-  <div classname = {styles.projbg}</div>
-  <div classname = {styles.navbar}
-    <h1><a href="/index" style="color: #000000;">Home</a></h1>
-    <h1><a href="/projects" style="color: #000000;">Projects</a></h1>
-    
+  return (
+    <div>
+      <div className={styles.projbg}></div>
+      <div className={styles.navbar}>
+        <h1>
+          <a href="/index" style={{ color: '#000000' }}>Home</a>
+        </h1>
+        <h1>
+          <a href="/projects" style={{ color: '#000000' }}>Projects</a>
+        </h1>
+      </div>
     </div>
-  
   );
-    
 }
