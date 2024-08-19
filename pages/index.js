d1152b3ef9a040e4178e18d614a8fbbea12ffd97
r-y4n <153890995+r-y4n@users.noreply.github.com>
2024-08-18 19:27:23 -0500

    aaa

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-if (/Mobi|Android|iPhone/i.test(globalThis.navigator.userAgent)) {
-    window.location.replace("/home");
+if (typeof window !== "undefined") {
+    if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+        window.location.replace("/home");
+    } else {
+        const messageDiv = document.createElement('div');
+        const messageHeader = document.createElement('h1');
+        messageHeader.textContent = "Sorry, This page cannot be viewed on your device.";
+        messageDiv.appendChild(messageHeader);
+        document.body.appendChild(messageDiv);
+    }
 } else {
-    const messageDiv = document.createElement('div');
-    const messageHeader = document.createElement('h1');
-    messageHeader.textContent = "Sorry, This page cannot be viewed on your device.";
-    messageDiv.appendChild(messageHeader);
-    document.body.appendChild(messageDiv);
+    // Code for server-side rendering if needed
+    console.log("Server-side rendering: Skipping user agent check");
 }
