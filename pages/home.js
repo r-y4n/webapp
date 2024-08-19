44e73c50f4b6dab2913a30cc58b7632650512301
r-y4n <153890995+r-y4n@users.noreply.github.com>
2024-08-18 19:16:08 -0500

    device checker

---
+++ pages/home.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react';
+import styles from '../styles/styles.module.css';
+
+const images = [
+  '/images/IMG_4028.jpeg',
+  '/images/IMG_4001.jpeg',
+  '/images/IMG_7634.jpeg'
+];
+
+export default function Home() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [fade, setFade] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(false);
+      setTimeout(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setFade(true);
+      }, 1000); // Match this duration to the CSS transition duration
+    }, 4000); // Change image every 4 seconds (including 1 second for fade out and 1 second for fade in)
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const footer = document.createElement('footer');
+    footer.innerHTML = `
+      <p><small>all code on this website and in <a href="https://github.com/r-y4n" style="color: #0000eb;">GitHub</a> made by ryan j.</small></p>
+      <p><small>Optimized for iPhone</small></p>
+    `;
+    footer.style.backgroundColor = '#333';
+    footer.style.color = '#d8d8ff';
+    footer.style.textAlign = 'center';
+    footer.style.padding = '10px';
+    footer.style.position = 'fixed';
+    footer.style.bottom = '0';
+    footer.style.width = '95%';
+    footer.style.height = 'auto'; // Adjust height as needed
+    document.body.appendChild(footer);
+
+    return () => {
+      document.body.removeChild(footer);
+    };
+  }, []);
+
+  return (
+    <div className={styles.indxcontainer}>
+      <img 
+        src={images[currentImageIndex]} 
+        alt="if you are seeing this i made an oopsie 😔🙏" 
+        className={`${styles.image} ${fade ? styles['fade-in'] : ''}`} 
+      />
+      <div className={styles.circ2}></div>
+      <div className={styles.circ1}></div>
+        <div className={styles.navbarind}>
+        <h1>
+          <a href="/index" style={{ color: '#000000' }}>Home</a>
+        </h1>
+        <h1>
+          <a href="/projects" style={{ color: '#000000' }}>Projects</a>
+        </h1>
+      </div>
+    </div>
+  );
+}
