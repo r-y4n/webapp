af4476afa73d2c0165c6e7a9bba9180a38a49a9c
r-y4n <153890995+r-y4n@users.noreply.github.com>
2024-08-18 19:21:52 -0500

    a

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 if(/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
-    window.location.replace("https://verycoolwebapp.vercel.app/home")}
+    window.location.replace("/home")}
 else {
     <div><h1>"Sorry, This page cannot be viewed on your device."</h1></div>
 }
