f383c2d0c85335033afc7e6731320bba74eb09f1
r-y4n <153890995+r-y4n@users.noreply.github.com>
2024-08-18 19:24:37 -0500

    a

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 
-if(/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+if(/Mobi|Android|iPhone/i.test(global.navigator.userAgent)) {
     window.location.replace("/home")}
 else {
     <div><h1>"Sorry, This page cannot be viewed on your device."</h1></div>
