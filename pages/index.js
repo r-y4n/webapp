de94e627342a000dce5fbb41d50d63cb598f014b
r-y4n <153890995+r-y4n@users.noreply.github.com>
2024-08-18 19:26:14 -0500

    abcdefghijklmnopqrstuvwxyz

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-
-if(/Mobi|Android|iPhone/i.test(global.navigator.userAgent)) {
-    window.location.replace("/home")}
-else {
-    <div><h1>"Sorry, This page cannot be viewed on your device."</h1></div>
+if (/Mobi|Android|iPhone/i.test(globalThis.navigator.userAgent)) {
+    window.location.replace("/home");
+} else {
+    const messageDiv = document.createElement('div');
+    const messageHeader = document.createElement('h1');
+    messageHeader.textContent = "Sorry, This page cannot be viewed on your device.";
+    messageDiv.appendChild(messageHeader);
+    document.body.appendChild(messageDiv);
 }
