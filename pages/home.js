9bc882885526f8612e08203a8be22c0da7ab5767
r-y4n <153890995+r-y4n@users.noreply.github.com>
2024-08-22 20:59:16 -0500

    Update home.js
    changed footer color (it's ugly)

--- pages/home.js
+++ pages/home.js
@@ -28,7 +28,7 @@ export default function Home() {
       <p><small>all code on this website and in <a href="https://github.com/r-y4n" style="color: #0000eb;">GitHub</a> made by ryan j.</small></p>
       <p><small>Optimized for iPhone</small></p>
     `;
-    footer.style.backgroundColor = '#8ec4e6';
+    footer.style.backgroundColor = '#d8ebff';
     footer.style.opacity = '80%';
     footer.style.color = '#d8d8ff';
     footer.style.textAlign = 'center';
